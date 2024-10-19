fe672e361fdaf4d31589ac636ba9263829e7bd2d
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2024-10-19 19:30:38 +0900

    add staging config yml

--- config/production.rb
+++
@@ -1,3 +0,0 @@
-set :application, "production"
-set :deploy_to, "/var/www/production"
-server '160.251.234.214', user: 'deploy', roles: %w{app db web}
--- config/staging.rb
+++
@@ -1,3 +0,0 @@
-set :application, "staging"
-set :deploy_to, "/var/www/staging"
-server '160.251.234.214', user: 'deploy', roles: %w{app db web}
